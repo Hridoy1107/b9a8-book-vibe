b7afd9879fc2a8bfbfdbf6f0a7e3e3ed3cdf5a9a
Hridoy1107 <hridoyx1107@gmail.com>
2024-03-26 19:43:09 +0600

    6th

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -3,7 +3,9 @@
 const Books = () => {
     return (
         <>
-        
+            <div className="mt-10">
+                <h1 className="font-semibold text-5xl text-center text-black">Books</h1>
+            </div>
         </>
     );
 };
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,9 +6,6 @@ const Home = () => {
     return (
         <>
             <Banner></Banner>
-            <div className="mt-10">
-                <h1 className="font-semibold text-5xl text-center text-black">Books</h1>
-            </div>
             <Books></Books>
         </>
     );
