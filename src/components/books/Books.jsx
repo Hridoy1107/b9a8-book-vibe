993dd26d42a9c6eda30643a8e6addd4836144434
Hridoy1107 <hridoyx1107@gmail.com>
2024-03-26 19:51:56 +0600

    7th

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 
 
 const Books = () => {
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        fetch('books.json')
+            .then(res => res.json())
+            .then(data => setBooks(data));
+    }, [])
+
     return (
         <>
             <div className="mt-10">
-                <h1 className="font-semibold text-5xl text-center text-black">Books</h1>
+                <h1 className="font-semibold text-5xl text-center text-black">Books {books.length}</h1>
+            </div>
+            <div>
+                
             </div>
         </>
     );
