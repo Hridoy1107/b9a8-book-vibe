89083732e97279e47580e6a5eb227dc68ba9c947
Hridoy1107 <hridoyx1107@gmail.com>
2024-03-26 20:59:21 +0600

    8th

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,26 +1,41 @@
-import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+import { CiStar } from "react-icons/ci";
 
-
-const Books = () => {
-
-    const [books, setBooks] = useState([])
-
-    useEffect(() => {
-        fetch('books.json')
-            .then(res => res.json())
-            .then(data => setBooks(data));
-    }, [])
-
+const Books = ({ book }) => {
+    const { bookName, tags, image, author, category, rating } = book
     return (
         <>
-            <div className="mt-10">
-                <h1 className="font-semibold text-5xl text-center text-black">Books {books.length}</h1>
-            </div>
-            <div>
-                
+            <div className="card h-[810px] w-[384px] bg-base-100 shadow-xl">
+                <img className="w-[320px] h-[550px]" src={image} alt="Shoes" />
+                <div className="card-body w-[320px] h-[156px]">
+                    <div className="flex gap-3">
+                        <div className="badge badge-accent ">{tags[0]}</div>
+                        <div className="badge badge-accent ">{tags[1]}</div>
+                    </div>
+                    <h2 className="card-title">
+                        {bookName}
+                    </h2>
+                    <h2 className="card-title font-normal">
+                        By: {author}
+                    </h2>
+                </div>
+                <hr />
+                <div className="card-body w-[320px] h-[92px]">
+                    <div className="flex justify-between">
+                        <h2 className="card-title font-medium text-xl">
+                            {category}
+                        </h2>
+                        <h2 className="card-title font-normal text-xl">
+                            {rating} <CiStar />
+                        </h2>
+                    </div>
+                </div>
             </div>
         </>
     );
 };
 
+Books.propTypes = {
+    book: PropTypes.array.isRequired
+};
 export default Books;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,26 @@
 import Banner from "../banner/Banner";
 import Books from "../books/Books";
-
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        fetch('books.json')
+            .then(res => res.json())
+            .then(data => setBooks(data));
+    }, [])
     return (
         <>
             <Banner></Banner>
-            <Books></Books>
+            <div className="my-10">
+                <h1 className="font-semibold text-5xl text-center text-black">Books {books.length}</h1>
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                {
+                    books.map(book => <Books key={book.bookId} book={book} ></Books>)
+                }
+            </div>
         </>
     );
 };
